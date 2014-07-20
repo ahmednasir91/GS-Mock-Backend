34bd913e6ded1fa0c623081b30a2f75b73aa322c
Ahmed Nasir <ahmednasir91@gmail.com>
2014-07-20 16:02:13 +0500

    Add synch data call

--- app.js
+++ app.js
@@ -35,6 +35,72 @@ app.post("/rest/doLogin", function(req, res) {
         });
 });
 
+app.get('/rest/synchData', function(req, res) {
+
+    var sessionId = req.body.session_id;
+    var lastUpdated = req.body.last_updated;
+
+    res.send({
+          "success": true,
+
+          "data": {
+             "current_time": "2014-07-14 19:43:37 +0500",
+             "locations": [
+                 {
+                   "id": 1,
+                   "name": "Name of Location",
+                   "wing": "Allowed Wing",
+                   "grades": [
+                       "V",
+                       "VI"
+                   ]
+                 },
+                 {
+                   "id": 2,
+                   "name": "Name of Location",
+                   "wing": "Allowed Wing",
+                   "grades": [
+                       "V",
+                       "VI"
+                   ]
+                 }
+               ],
+             "students": [
+                 {
+                   "id": 1,
+                   "tag_id": "abcdefgh",
+                   "gs_number": "010-1000",
+                   "grade": "V",
+                   "gen_group": "B",
+                   "wing": "Middlers",
+                   "official_name": "Shahzad Fateh Ali",
+                   "image": "base64 encoded image"
+                 },
+                 {
+                   "id": 2,
+                   "tag_id": "abcdefgh",
+                   "gs_number": "010-1000",
+                   "grade": "V",
+                   "gen_group": "B",
+                   "wing": "Middlers",
+                   "official_name": "Shahzad Fateh Ali",
+                   "image": "base64 encoded image"
+                 }
+               ],
+             "schedules": [
+                 {
+                   "grade": "V",
+                   "late_time": "2014-07-14 19:43:37 +0500"
+                 },
+                 {
+                   "grade": "VI",
+                   "late_time": "2014-07-14 19:43:37 +0500"
+                 }
+               ]
+          }
+    });
+});
+
 var port = Number(process.env.PORT || 5000);
 console.log("Listening on " + port + " ...")
 app.listen(port);
